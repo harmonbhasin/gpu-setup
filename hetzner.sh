ad850625c0c56929dec2a2127b5701fe64b80b29
harm0n <harmonprograms@protonmail.com>
2026-04-05 13:54:55 +0000

    removing tar from working dir

--- hetzner.sh
+++ hetzner.sh
@@ -20,6 +20,7 @@ cargo install ripgrep tree-sitter-cli
 # neovim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+rm nvim-linux-x86_64.tar.gz
 echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> ~/.bashrc
 
 # claude
