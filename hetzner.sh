058f8c208eac48da9f0a0178f671a07da65ef447
harm0n <harmonprograms@protonmail.com>
2026-04-05 15:50:20 +0000

    adding a way to see stuff better

--- hetzner.sh
+++ hetzner.sh
@@ -4,7 +4,7 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 
 # Other stuff
 apt update
-apt install -y vim gh npm zoxide fzf tmux htop redis-server lsof net-tools iperf3 jq strace kitty-terminfo libclang-dev python3.12-venv
+apt install -y vim gh npm zoxide fzf tmux htop redis-server lsof net-tools iperf3 jq strace kitty-terminfo libclang-dev python3.12-venv python3-pip btop
 
 # NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
