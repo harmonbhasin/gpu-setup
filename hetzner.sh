1ebd881f7200569324ce1e8d96a83a023e3877e2
harm0n <harmonprograms@protonmail.com>
2026-06-23 17:43:04 +0000

    Remove vim as i like ctrl+j for codex

--- hetzner.sh
+++ hetzner.sh
@@ -124,13 +124,7 @@ set -g mouse on
 
 # Reload config
 bind r source-file ~/.tmux.conf \; display-message "Config reloaded."
-
-# Vim-tmux navigation
-is_vim="ps -o state= -o comm= -t '\'#{pane_tty}\'' | grep -iqE '\''^[^TXZ ]+ +(\\S+\\/)?g?(view|n?vim?x?)(diff)?$'\''"
-bind-key -n '\''C-h'\'' if-shell "$is_vim" '\''send-keys C-h'\'' '\''select-pane -L'\''
-bind-key -n '\''C-j'\'' if-shell "$is_vim" '\''send-keys C-j'\'' '\''select-pane -D'\''
-bind-key -n '\''C-k'\'' if-shell "$is_vim" '\''send-keys C-k'\'' '\''select-pane -U'\''
-bind-key -n '\''C-l'\'' if-shell "$is_vim" '\''send-keys C-l'\'' '\''select-pane -R'\''' > ~/.tmux.conf
+> ~/.tmux.conf
 
 mkdir -p ~/.config
 mkdir -p ~/.claude
